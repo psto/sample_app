fc209e37472acd94d9a553dc82c9a8ed3b9055df
Piotr Stojanow <stojanpro@gmail.com>
2014-07-28 19:51:00 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  resources :users
   root  'static_pages#home'
+
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
